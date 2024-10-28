bfc64c7a344b9dc011d26f7b7d0aec13b3397adc
Omar.Abdul <omarabdul471@gmil.com>
2024-10-28 17:14:31 +0500

    Доделать

---
+++ Task7.1/Task7.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Posit(5, -3));
+            Console.WriteLine(Posit(-1, -2));
+            Console.WriteLine(Posit(0, 4));
+            Console.WriteLine(Posit(-7, 0));
+        }
+        static void Posit(int m, int n) 
+        {
+        int resuilt = (m > 0) ? m : (n>0) ? n : 0;
+            Console.WriteLine(resuilt);
+        }
+    }
+}
