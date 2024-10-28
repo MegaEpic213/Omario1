dc40c8f55095c9f82d27724ac71fbc974e00d07c
Omar.Abdul <omarabdul471@gmil.com>
2024-10-28 16:46:36 +0500

    Выполнено

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x:");
+            var x = double.Parse(Console.ReadLine());
+            var y = Math.Sqrt(x + 1/Math.Sqrt(Math.Pow(x,2)+4));
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
