565905b492d25146b9f3de1934c32f43efb8dbfc
Omar.Abdul <omarabdul471@gmil.com>
2024-11-11 17:05:19 +0500

    Задание 8 выполнено

--- Task8/Task8/Program.cs
+++ Task8/Task8/Program.cs
@@ -22,11 +22,11 @@ namespace Task8
             {
             if (x > 0)
                 return 1;
-            else if (x < 1)
+            else if (x < 0)
                 return -1;
-            else if (x != 0)
+            else
                 return 0;
-                    return x;
+         
             }
         }
     }
