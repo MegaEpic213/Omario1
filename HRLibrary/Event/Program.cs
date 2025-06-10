23335bdc017f2cf73a1cc0a094ea64eb2fa1a9ab
Омар Абдул <omarabdul471@gmail.com>
2025-06-10 22:55:18 +0500

    Задание 14 выполнено

---
+++ HRLibrary/Event/Program.cs
@@ -0,0 +1,33 @@
+﻿using HRLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event[] events = new Event[]
+            {
+                new Birthday(new DateTime(2023, 12, 15, 18, 0, 0),
+                    "Кафе 'Уют'", "Иван Иванов", 1990),
+                new Meeting(new DateTime(2023, 12, 16, 10, 30, 0),
+                    "Офис, каб. 305", "Алексей Петров", "Обсуждение проекта"),
+                new Birthday(new DateTime(2023, 12, 20, 19, 0, 0),
+                    "Дома", "Мария Сидорова", 1985)
+            };
+
+            Console.WriteLine("НАПОМИНАНИЯ:\n");
+            foreach (var ev in events)
+            {
+                ev.Remind();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
