308f93797801edc7e94630bc42684464964b2ce8
Омар Абдул <omarabdul471@gmail.com>
2025-06-11 04:31:13 +0500

    Задание 16 выполнено

---
+++ Structura/Structura/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorStruct
+{
+    public struct Vector3D
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        public double Length => Math.Sqrt(X * X + Y * Y + Z * Z);
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString() => $"({X}, {Y}, {Z})";
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Vector3D))
+                return false;
+
+            var other = (Vector3D)obj;
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+                hash = hash * 23 + Z.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static bool operator ==(Vector3D a, Vector3D b) => a.Equals(b);
+        public static bool operator !=(Vector3D a, Vector3D b) => !a.Equals(b);
+
+        public static Vector3D operator +(Vector3D a, Vector3D b) =>
+            new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static Vector3D operator -(Vector3D a, Vector3D b) =>
+            new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public static Vector3D operator *(Vector3D v, double scalar) =>
+            new Vector3D(v.X * scalar, v.Y * scalar, v.Z * scalar);
+
+        public static Vector3D operator *(double scalar, Vector3D v) => v * scalar;
+
+        public static double operator *(Vector3D a, Vector3D b) =>
+            a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+
+        public static Vector3D operator ^(Vector3D a, Vector3D b) =>
+            new Vector3D(
+                a.Y * b.Z - a.Z * b.Y,
+                a.Z * b.X - a.X * b.Z,
+                a.X * b.Y - a.Y * b.X
+            );
+    }
+}
